c305534d8f10277e8945ca2ef76e217940eacfd3
kudou <h.kudou.sys22@morijyobi.ac.jp>
2022-09-27 11:01:42 +0900

    100回Hello World
    Hello Worldを１００回出力に変更

--- pra2.java
+++ pra2.java
@@ -1,3 +1,7 @@
-for(int i = 1; i <= 100; i++){
-	System.out.println("Hello World");
+public class pra2{
+	public static void main(String[] args){
+		for(int i = 1; i <= 100; i++){
+			System.out.println("Hello World");
+		}
+	}
 }
